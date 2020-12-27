7fcb83a38fc47ff20c717277b07af1922b3b9667
Дима <knyazevdima05@gmail.com>
2020-12-27 12:31:01 +0300

    change topic

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import pygame as pg
 import pygame_gui
-from all_sprites import Leaders
-from all_sprites import Menu
-from all_sprites import ScoreDisplay
 from all_sprites import menu_screen
 
 pg.init()
 
-pg.display.set_caption("2048 2.0")  # заголовок
+pg.display.set_caption(" ")  # заголовок
 
-size = width, height = 360, 540
+size = width, height = 800, 600
 
 screen = pg.display.set_mode(size, pg.RESIZABLE)
 color = "white"
@@ -27,23 +24,7 @@ changing_pos = False
 
 manager = pygame_gui.UIManager((width, height))
 
-def draw_logo():
-    text_logo = "2048"
-    font = pg.font.Font(None, 60)
-    text = font.render(text_logo, 1, (0, 200, 0))
-    screen.blit(text, (10, 10))
-
-
 ui_sprites = pg.sprite.Group()
-
-score = "0"
-score_board = ''
-def draw_all_ui():
-    global score_board
-    score_board = ScoreDisplay(ui_sprites, 123, 10, 100, 70, score)
-    Menu(ui_sprites, 230, 10, 100, 30, screen)
-    Leaders(ui_sprites, 230, 50, 100, 30)
-    draw_logo()
 
 
 def update_score(delta):
@@ -52,11 +33,10 @@ def update_score(delta):
     score += delta
     score = str(score)
 
-draw_all_ui()
+
 
 while run:
     screen.fill(pg.Color(color))
-    draw_logo()
     time_delta = clock.tick(60) / 1000.0
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -74,10 +54,7 @@ while run:
             if event.user_type == pygame_gui.UI_CONFIRMATION_DIALOG_CONFIRMED:
                 run = False
         if event.type == pg.MOUSEBUTTONUP:
-            update_score(100)
-            score_board = ScoreDisplay(ui_sprites, 123, 10, 100, 70, score)
-            coords = event.pos
-            ui_sprites.update(event)
+            pass
     ui_sprites.draw(screen)
 
     pg.display.flip()  # Обновление кадра

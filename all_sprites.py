bfa2f6b22a7056fd2ef474f68fe19f2ec99e72b0
Дима <knyazevdima05@gmail.com>
2020-12-27 22:08:17 +0300

    Изменение темы и добавление физика газов

--- all_sprites.py
+++ all_sprites.py
@@ -1,32 +1,45 @@
 import pygame as pg
 import sys
+import os
+import random
+
+horizontal_borders = pg.sprite.Group()
+vertical_borders = pg.sprite.Group()
+all_sprites = pg.sprite.Group()
+molecule_group = pg.sprite.Group()
+
+class Molecule(pg.sprite.Sprite):
+    """Класс молекулы"""
+
+    def __init__(self, x, y, radius, color):
+        super().__init__(all_sprites, molecule_group)
+        self.radius = radius
+        self.image = pg.Surface((2 * radius, 2 * radius),
+                                pg.SRCALPHA, 32)
+        pg.draw.circle(self.image, color,
+                       (radius, radius), radius)
+        self.rect = pg.Rect(x, y, 2 * radius, 2 * radius)
+        self.vx = random.randint(-8, 0)
+        self.vy = random.randrange(-8, 0)
+
+    def update(self):
+        """Обновление положения молекулы, а также скорость в зависимости от скорости(уже реальной, но она слишком огромная)"""
+        self.rect = self.rect.move(self.vx, self.vy)
+        if pg.sprite.spritecollideany(self, horizontal_borders):
+            self.vy = -self.vy
+        if pg.sprite.spritecollideany(self, vertical_borders):
+            self.vx = -self.vx
 
 
-
-
-
-
-def terminate():
-    pg.quit()
-    sys.exit()
-
-def menu_screen(screen):
-    intro_text = ''
-
-    menu = pg.Surface((360, 540), pg.SRCALPHA, 32)
-    font = pg.font.Font(None, 30)
-    text_coord = 50
-    menu.blit(screen, (0, 0))
-    for line in intro_text:
-       pass
-    run = True
-    while True:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                terminate()
-            elif event.type == pg.KEYDOWN or \
-                    event.type == pg.MOUSEBUTTONDOWN:
-                return  # начинаем игру
-        pg.display.flip()
-
-
+class Border(pg.sprite.Sprite):
+    # строго вертикальный или строго горизонтальный отрезок
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__(all_sprites)
+        if x1 == x2:  # вертикальная стенка
+            self.add(vertical_borders)
+            self.image = pg.Surface([1, y2 - y1])
+            self.rect = pg.Rect(x1, y1, 1, y2 - y1)
+        else:  # горизонтальная стенка
+            self.add(horizontal_borders)
+            self.image = pg.Surface([x2 - x1, 1])
+            self.rect = pg.Rect(x1, y1, x2 - x1, 1)
--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import pygame as pg
+import math
 import pygame_gui
 import os
 import sys
-from all_sprites import menu_screen
+import random
+from all_sprites import Molecule, Border, all_sprites, molecule_group, horizontal_borders, vertical_borders
 
 pg.init()
 
-pg.display.set_caption(" ")  # заголовок
+pg.display.set_caption("Симулятор газа")  # заголовок
 
 size = width, height = 800, 600
 
-screen = pg.display.set_mode(size, pg.RESIZABLE)
-color = "white"
-screen.fill(pg.Color(color))
+screen = pg.display.set_mode(size)
+
 pg.display.update()
-count = 0
+
 run = True  # переменна, с помощью ее можно выходить из цикла
 
 """А вот тут будет волшебство)"""
@@ -23,10 +24,17 @@ clock = pg.time.Clock()
 RED = (255, 0, 0)
 
 changing_pos = False
+BLACK = (0, 0, 0)
 
-manager = pygame_gui.UIManager((width, height))
+"""Физичиские величины"""
+T = 100  # K
+G = 9.8  # м/с**2
+P = 101325  # Па, давление воздуха
+M = 4.82 * (10 ** -26)  # масса малекулы воздуха
+N = 0
+K = 1.38 * (10 ** -23)  # Постоянная Больцмана
 
-ui_sprites = pg.sprite.Group()
+
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
     # если файл не существует, то выходим
@@ -37,33 +45,83 @@ def load_image(name, colorkey=None):
     return image
 
 
+# Немного теории, мы делаем идеальный газ, где молекулы не сталкиваются друг с другом
+
+
+Border(5, 5, width - 5, 5)
+Border(5, height - 5, width - 5, height - 5)
+Border(5, 5, 5, height - 5)
+Border(width - 5, 5, width - 5, height - 5)
+
 
 def fon():
     fon = pg.transform.scale(load_image('blue-snow.png'), (width, height))
     screen.blit(fon, (0, 0))
 
 
+def create_molecule():
+    """Создает по 15 молекул"""
+    global N
+    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    for i in range(15):
+        Molecule(width - 100, height - 100, 20, color)
+    N += 15
+
+
+def calculating_the_average_speed_of_molecules():
+    """Рассчитывает среднюю квадратичную скорость молекул"""
+    V = math.sqrt((3 * K * T) / M)
+    return V
+
+
+def calculating_the_pressure():
+    """Возвращает давление, которое газ оказывает на стенки"""
+    P = (N * M * pow(calculating_the_average_speed_of_molecules(), 2)) / 3
+    return P
+
+
+def change_t(num):
+    """Эта функция увеличивает температуру в кельвинах на 10 кельвинов"""
+    global T
+    T += num
+
+
+def show_parametrs():
+    font = pg.font.Font(None, 30)
+    text = font.render(str(f"Температура: {T}K/ {round(T - 273.15)}C"), 1, BLACK)
+    screen.blit(text, (width - 300, 10))
+    text = font.render(str(f"Скорость молекул: {round(calculating_the_average_speed_of_molecules())}м/с"), 1, BLACK)
+    screen.blit(text, (width - 300, 40))
+    text = font.render(str(f"Давление: {calculating_the_pressure()}Па"), 1,
+                       BLACK)
+    screen.blit(text, (10, 10))
+
+
+SPEED = 60
+
+create_molecule()
 while run:
     fon()
-    time_delta = clock.tick(60) / 1000.0
+    all_sprites.update()
+    all_sprites.draw(screen)
+    time_delta = clock.tick(SPEED) / 1000.0
     for event in pg.event.get():
         if event.type == pg.QUIT:
-            confirmation_dialog = pygame_gui.windows.UIConfirmationDialog(
-                rect=pg.Rect((250, 200), (300, 200)),
-                manager=manager,
-                window_title="Подтверждение",
-                action_long_desc="Вы точно хотите выйти?",
-                action_short_name="Ok",
-                blocking=True
-            )
             run = False
 
-        if event.type == pg.USEREVENT:
-            if event.user_type == pygame_gui.UI_CONFIRMATION_DIALOG_CONFIRMED:
-                run = False
         if event.type == pg.MOUSEBUTTONUP:
-            pass
-    ui_sprites.draw(screen)
+            if event.button == 1:
+                create_molecule()
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_DOWN:
+                if T > 10:
+                    change_t(-10)
+                    SPEED -= 5
+            if event.key == pg.K_UP:
+                change_t(10)
+                SPEED += 5
+
+    show_parametrs()
 
     pg.display.flip()  # Обновление кадра
 pg.quit()
